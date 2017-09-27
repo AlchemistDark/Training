b68481ba0137801c1129b50fa1b7a4a208e3206d
Alchemist_Dark <justplus@rambler.ru>
2017-09-27 15:39:36 +0700

    alpha 0.2

--- Scheduler.py
+++ Scheduler.py
@@ -1,8 +1,17 @@
-﻿#Программа, которая ведёт список дел
-#Для начала - создание списка
+﻿#Scheduler alpha 0.2 (консольная версия)
+#Сценарий, которй ведёт список дел
+#Сценарий написан для Python3, так как новый стиль форматирования строк
+#интереснее, но вероятно, целесообразно сделать и для 2.6--2.7
 
-print('Scheduler alpha 0.1 (консольная версия)')#Шапка
-plan  = []					#Основной список
+def tasker(task):		#создаёт строку (словарь, dikt) c заданием
+    __task={'start_time':task[0], 'end_time':task[1], 'plase':task[2], 'description':task[3]}
+    return __task
+
+#class Task(list):
+#    pass
+
+print('Scheduler alpha 0.2 (консольная версия)')	#Шапка
+plans  = []			#Основной список
 
 #Основной цикл (позже, может, имеет смысл сделать его процедурой)
 
@@ -16,14 +25,22 @@ while True:
 ''')
 
     if   mode == "in":
-        plan.append(input('Введите новый элемент списка\n'))
+        task=input(
+'Введите через запятую время начала, время окончания, место и название события\n'
+).split(',')
+        plans.append(tasker(task))
     elif mode == "out":
         print ('Весь список:\n')
-        print (plan)
+        print ('{0:<20}{1:<20}{2:<20}{3:<20}'.format(
+            'Время начала','Время окончания','Место','Описание'))
+        for plan in plans:
+            print ('{0[start_time]:<20}{0[end_time]:<20}{0[plase]:<20}{0[description]:<20}'.format(plan))
     elif mode == "q":
         break
     else: 
         print('Что-то не так')
         continue
+
+
 
 
